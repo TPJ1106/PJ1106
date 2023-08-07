bab33d2a65fa834edd7cb8e8a2864bbd432ea9cc
Mystic-707 <smyu2001@gmail.com>
2023-08-07 19:53:50 +0900

    reset

---
+++ NewGP/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+//import 'react-native-reanimated'
+
+export default function App() {
+  function HelpScreenHandler(){
+
+  };
+
+  return (
+    <View>
+      <View style={styles.forHelpButton}>
+        <Button title='HELP'/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  forAll: {
+    flexDirection: 'row'
+  },
+  forHelpButton: {
+    marginTop: 40,
+    marginLeft: '85%',
+    width: '15%',
+    alignItems: 'center',
+  },
+});
