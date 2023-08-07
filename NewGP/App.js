cb6062d51aeb24db403ff8d02db7e538f48911ee
Mystic-707 <smyu2001@gmail.com>
2023-08-07 23:28:18 +0900

    Soudplaycomplete

--- NewGP/App.js
+++ NewGP/App.js
@@ -1,16 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-//import 'react-native-reanimated'
+import * as React from'react'
+import { Audio } from 'expo-av'
+
 
 export default function App() {
   function HelpScreenHandler(){
 
   };
 
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    try {
+      console.log('Loading Sound');
+      const { sound } = await Audio.Sound.createAsync( require('./assets/TestSound.wav'));
+      setSound(sound);
+  
+      console.log('Playing Sound');
+      await sound.playAsync();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View>
       <View style={styles.forHelpButton}>
-        <Button title='HELP' onPress={HelpScreenHandler}/>
+        <Button title='HELP' onPress={playSound}/>
       </View>
     </View>
   );
